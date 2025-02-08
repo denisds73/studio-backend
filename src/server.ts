2dde52e117a6459690e6afbe1f45707ed87bc4e2
Flavio Denis <flavio.d@commerceiq.ai>
2025-02-08 18:15:41 +0530

    Demo data removed

--- src/server.ts
+++ src/server.ts
@@ -8,34 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-//the below is data feed - used in the begginit .. hee hee
-
-// const data = [
-//   {
-//     name: "Basic",
-//     price: "100",
-//     features: ["Feature 1", "Feature 2", "Feature 3"],
-//   },
-//   {
-//     name: "Standard",
-//     price: "200",
-//     features: ["Feature 1", "Feature 2", "Feature 3"],
-//   },
-//   {
-//     name: "Premium",
-//     price: "300",
-//     features: ["Feature 1", "Feature 2", "Feature 3"],
-//   },
-// ];
-
-// data.forEach(async (pkg) => {
-//   await fetch('http://localhost:3000/api/packages', {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify(pkg),
-//   });
-// });
-
 initDB().then((db) => {
   app.use('/api/packages', packageRoutes(db));
 
